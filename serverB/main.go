9d4381b51cd10c0e01db2a8bd79bfeb4974d673a
黄小旭 <huangxx@vstone.com.cn>
2018-04-20 19:46:00 +0800

    add grpc

---
+++ serverB/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"text/tabwriter"
+
+	"github.com/oklog/oklog/pkg/group"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"google.golang.org/grpc"
+	"github.com/go-kit/kit/log"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	kitgrpc "github.com/go-kit/kit/transport/grpc"
+	"github.com/go-kit/kit/metrics/prometheus"
+	"github.com/go-kit/kit/metrics"
+
+	"github.com/suyuanhxx/go-kit-demo/serverB/addendpoint"
+	"github.com/suyuanhxx/go-kit-demo/serverB/addtransport"
+	"github.com/suyuanhxx/go-kit-demo/serverB/addservice"
+	"github.com/suyuanhxx/go-kit-demo/serverB/pb"
+)
+
+func main() {
+	// Define our flags. Your service probably won't need to bind listeners for
+	// *all* supported transports, or support both Zipkin and LightStep, and so
+	// on, but we do it here for demonstration purposes.
+	fs := flag.NewFlagSet("addsvc", flag.ExitOnError)
+	var (
+		grpcAddr = fs.String("grpc-addr", ":8082", "gRPC listen address")
+	)
+	fs.Usage = usageFor(fs, os.Args[0]+" [flags]")
+	fs.Parse(os.Args[1:])
+
+	// Create a single logger, which we'll use and give to other components.
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
+
+	// Create the (sparse) metrics we'll use in the service. They, too, are
+	// dependencies that we pass to components that use them.
+	var ints, chars metrics.Counter
+	{
+		// Business-level metrics.
+		ints = prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "example",
+			Subsystem: "addsvc",
+			Name:      "integers_summed",
+			Help:      "Total count of integers summed via the Sum method.",
+		}, []string{})
+		chars = prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "example",
+			Subsystem: "addsvc",
+			Name:      "characters_concatenated",
+			Help:      "Total count of characters concatenated via the Concat method.",
+		}, []string{})
+	}
+	var duration metrics.Histogram
+	{
+		// Endpoint-level metrics.
+		duration = prometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+			Namespace: "example",
+			Subsystem: "addsvc",
+			Name:      "request_duration_seconds",
+			Help:      "Request duration in seconds.",
+		}, []string{"method", "success"})
+	}
+	http.DefaultServeMux.Handle("/metrics", promhttp.Handler())
+
+	// Build the layers of the service "onion" from the inside out. First, the
+	// business logic service; then, the set of endpoints that wrap the service;
+	// and finally, a series of concrete transport adapters. The adapters, like
+	// the HTTP handler or the gRPC server, are the bridge between Go kit and
+	// the interfaces that the transports expect. Note that we're not binding
+	// them to ports or anything yet; we'll do that next.
+	var (
+		service    = addservice.New(logger, ints, chars)
+		endpoints  = addendpoint.New(service, logger, duration, nil, nil)
+		grpcServer = addtransport.NewGRPCServer(endpoints, nil, nil, logger)
+	)
+
+	// Now we're to the part of the func main where we want to start actually
+	// running things, like servers bound to listeners to receive connections.
+	//
+	// The method is the same for each component: add a new actor to the group
+	// struct, which is a combination of 2 anonymous functions: the first
+	// function actually runs the component, and the second function should
+	// interrupt the first function and cause it to return. It's in these
+	// functions that we actually bind the Go kit server/handler structs to the
+	// concrete transports and run them.
+	//
+	// Putting each component into its own block is mostly for aesthetics: it
+	// clearly demarcates the scope in which each listener/socket may be used.
+	var g group.Group
+	{
+		// The gRPC listener mounts the Go kit gRPC server we created.
+		grpcListener, err := net.Listen("tcp", *grpcAddr)
+		if err != nil {
+			logger.Log("transport", "gRPC", "during", "Listen", "err", err)
+			os.Exit(1)
+		}
+		g.Add(func() error {
+			logger.Log("transport", "gRPC", "addr", *grpcAddr)
+			// we add the Go Kit gRPC Interceptor to our gRPC service as it is used by
+			// the here demonstrated zipkin tracing middleware.
+			baseServer := grpc.NewServer(grpc.UnaryInterceptor(kitgrpc.Interceptor))
+			pb.RegisterAddServer(baseServer, grpcServer)
+			return baseServer.Serve(grpcListener)
+		}, func(error) {
+			grpcListener.Close()
+		})
+	}
+
+	logger.Log("exit", g.Run())
+}
+
+func usageFor(fs *flag.FlagSet, short string) func() {
+	return func() {
+		fmt.Fprintf(os.Stderr, "USAGE\n")
+		fmt.Fprintf(os.Stderr, "  %s\n", short)
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "FLAGS\n")
+		w := tabwriter.NewWriter(os.Stderr, 0, 2, 2, ' ', 0)
+		fs.VisitAll(func(f *flag.Flag) {
+			fmt.Fprintf(w, "\t-%s %s\t%s\n", f.Name, f.DefValue, f.Usage)
+		})
+		w.Flush()
+		fmt.Fprintf(os.Stderr, "\n")
+	}
+}
